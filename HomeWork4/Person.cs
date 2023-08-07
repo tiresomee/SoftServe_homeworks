b3e2e251941e7664759792d86d65bbeaac3fdd55
Natalia <85500617+tiresomee@users.noreply.github.com>
2023-08-07 15:26:00 +0300

    Person

---
+++ HomeWork4/Person.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Cache;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork4
+{
+    internal class Person
+    {
+        private string name;
+        private DateTime birthYear;
+
+        public string Name
+        { get { return name; } }
+
+        public DateTime BirthYear
+        { get { return birthYear; } }
+
+        public Person ()
+        {
+            name = "Natalia";
+            birthYear = new DateTime(2003, 5, 1);
+        }
+
+       public Person (string name, DateTime birthYear)
+        {
+            this.name = name;
+            this.birthYear = birthYear;
+        }
+
+        public int Age()
+        {
+            DateTime currentDate = DateTime.Now;
+            int age = currentDate.Year - birthYear.Year;
+            return age;
+
+        }
+
+        public static Person Input(int i)
+        {
+            Console.Write($"Enter the name of {i + 1} person: ");
+            string name = Console.ReadLine();
+            Console.Write($"Enter the birthday year of {i + 1} person: ");
+            DateTime birthYear = Convert.ToDateTime( Console.ReadLine() );
+            Person person = new Person(name, birthYear);  
+            return person;
+        }
+
+        public void ChangeName(string newName)
+        {
+            name = newName;
+        }
+
+        public override string ToString()
+        {
+            return $"Name of person: {name}, Age: {Age()}";
+        }
+
+        public void Output()
+        {
+            Console.WriteLine(ToString());
+        }
+
+        public static bool operator == (Person first, Person second)
+        {
+            return first.name == second.name;
+        }
+
+        public static bool operator != (Person first, Person second)
+        {
+            return first.name != second.name;
+        }
+
+
+    }
+}
---
+++ HomeWork4/Program.cs
@@ -0,0 +1,69 @@
+﻿
+namespace HomeWork4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Information about person with default constructor: ");
+            Person person1 = new Person();
+            person1.Output();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Information about person using constructor with 2 parameters: ");
+            DateTime birthDate = new DateTime(2000, 6, 15);
+            Person person2 = new Person("Oleg", birthDate);
+            person2.Output();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Enter information about 5 persons: ");
+            Person[] persons = new Person[5];
+            for ( int i = 0; i< persons.Length; i++ )
+            {
+                persons[i] = Person.Input(i);
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < persons.Length; i++)
+            {
+                for (int j = i + 1; j < persons.Length; j++)
+                {
+                    if (persons[i] == persons[j])
+                    {
+                        Console.WriteLine($"{persons[i]} and {persons[j]} have the same name");
+                    }
+
+                }
+
+            }
+
+            Console.WriteLine();
+
+            foreach (var person in persons)
+            {
+                if (person.Age() < 16)
+                {
+                    person.ChangeName("Very Young");
+                }             
+            }
+
+            Console.WriteLine("New information about persons: ");
+            foreach (var person in persons)
+            {
+                Console.WriteLine(person.ToString());
+            }
+
+        }
+        
+          
+   
+
+
+
+    }
+    
+}
+
